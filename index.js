3f8b3d8da1550b094481add91564990847286c6a
paul <paalochoame@ittepic.edu.mx>
2023-12-23 21:59:41 -0700

    cors40

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors=require('cors');
+const carrosRouter = require('./routers/carros.router');
+const usersRouter =  require('./routers/users.router');
 
 require('./utils/mongoConnection');
 
 
 
-const carrosRouter = require('./routers/carros.router');
-const usersRouter =  require('./routers/users.router');
+
 const app = express();
 const port = 3003;
-
 app.use(morgan('dev'));
-app.options('*', cors());
 
 app.use(cors());
 
@@ -24,6 +23,7 @@ app.get("/",(req,res)=>{
 app.use(express.json({limit: '50mb'}));
 app.use('/carros',carrosRouter);
 app.use('/users',usersRouter);
+
 app.listen(port,()=>{
     console.log("Servidor iniciado en http://localhost:" + port);
 })
