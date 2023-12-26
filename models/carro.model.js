9cd106762720ab51791381862c0bf93de121379b
paul <paalochoame@ittepic.edu.mx>
2023-12-26 12:40:48 -0700

    corrigiendo modelo auto

--- models/carro.model.js
+++ models/carro.model.js
@@ -4,8 +4,7 @@ const carroSchema = new mongoose.Schema({
     modelo: { type: String},
     color: { type: String},
     precio: { type: Number},
-    descripcion: { type: String},
-    linkimg: { type: String}      
+    descripcion: { type: String}    
 });
 
 const Carro = mongoose.model('Carro',carroSchema,'carros');
