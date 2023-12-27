0cd3e7097dc16e91e7ff124f428d9ccd05c2bb4c
IvanRobles19 <107086571+IvanRobles19@users.noreply.github.com>
2023-12-26 22:10:43 -0700

    proteccion de rutas

--- routers/carros.router.js
+++ routers/carros.router.js
@@ -3,14 +3,14 @@ const router=express.Router();
 const carroController=require('../controllers/carros.controller');
 const authMiddleware = require('../utils/auth.middleware');
 
-router.get("/",carroController.getCarros);
+router.get("/",authMiddleware.authenticateToken,carroController.getCarros);
 
-router.get("/:carroId",carroController.getCarrosId);
+router.get("/:carroId",authMiddleware.authenticateToken,carroController.getCarrosId);
 
 router.post("/",authMiddleware.authenticateToken,carroController.newCarro);
 
-router.put("/:carroId",carroController.updateCarro);
+router.put("/:carroId",authMiddleware.authenticateToken,carroController.updateCarro);
 
-router.delete("/:carroId",carroController.deleteCarro);
+router.delete("/:carroId",authMiddleware.authenticateToken,carroController.deleteCarro);
 
 module.exports=router;
