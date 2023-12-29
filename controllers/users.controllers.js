0e4ec2c56a3f75c9092cbcac65f1b369b5588114
paul <paalochoame@ittepic.edu.mx>
2023-12-29 13:14:05 -0700

    router

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -4,13 +4,13 @@ const User = require('../models/user.model');
 
 exports.registerUser = async (req,res) =>{
     try{
-        const{userName,email,password} = req.body;
+        const{userName,email,password,name,lastname} = req.body;
         const existingUser = await User.findOne({email});
         if(existingUser){
             return res.status(400).json({error: 'El usuario ya existe'})
         }
         const hashedPassword = await bcrypt.hash(password,10);
-        const newUser = new User ({userName,email,password: hashedPassword});
+        const newUser = new User ({userName,email,name,lastname,password: hashedPassword});
         await newUser.save();
 
         return res.status(201).json({message: 'Usuario registrado exitosamente'});
